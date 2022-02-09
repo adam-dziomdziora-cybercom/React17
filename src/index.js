8b252d20e2fd3e3c0ea39ea032f8701fad1871c5
Adam Dziomdziora <adam.dziomdziora@knowit.eu>
2022-02-09 10:35:07 +0100

    d1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import ReactDOM from 'react-dom';
 import '../style/main.scss';
 
 const HelloWorld = () => {
-  return <h1 className="header">Hi!</h1>;
+  return (
+    // <></>
+    <React.Fragment>
+      <h1 className="header">Hi!</h1>
+      <div id="injectHere" />
+      <div id="dupa" />
+    </React.Fragment>
+  );
 };
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+ReactDOM.render(
+  React.createElement(
+    'h2',
+    { id: 'mojeh2', className: 'header' },
+    'Playing with react is cool'
+  ),
+  document.getElementById('injectHere')
+);
+
+ReactDOM.render(
+  React.createElement(
+    'ul',
+    { id: 'fajnalistaReactowa' },
+
+    React.createElement('li', { id: 'item1' }, 'item 1 hi \u26CF;')
+  ),
+  document.getElementById('dupa')
+);
