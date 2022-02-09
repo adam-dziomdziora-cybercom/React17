62208cc004ee120838934b3042ae7eebb444eb4a
Adam Dziomdziora <adam.dziomdziora@knowit.eu>
2022-02-09 07:45:59 +0100

    s2

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1 className="header">Hi!</h1>;
+};
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
